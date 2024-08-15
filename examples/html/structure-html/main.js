03665fe206f624b666777f6be024a4dc2e5fb9b4
Dang nguyen <113990590+Yoriichi-Dang@users.noreply.github.com>
2024-08-15 11:15:40 +0700

    feature/47-config-project: add prettier format code

--- examples/html/structure-html/main.js
+++ examples/html/structure-html/main.js
@@ -1,22 +1,22 @@
 // Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Change header background color on button click
-  const changeColorButton = document.createElement("button");
-  changeColorButton.textContent = "Change Header Color";
-  document.querySelector("header").appendChild(changeColorButton);
+document.addEventListener('DOMContentLoaded', function () {
+    // Change header background color on button click
+    const changeColorButton = document.createElement('button');
+    changeColorButton.textContent = 'Change Header Color';
+    document.querySelector('header').appendChild(changeColorButton);
 
-  changeColorButton.addEventListener("click", function () {
-    document.querySelector("header").style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
-  });
-
-  // Smooth scroll to section on nav link click
-  document.querySelectorAll("nav a").forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      document
-        .querySelector(this.getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth" });
+    changeColorButton.addEventListener('click', function () {
+        document.querySelector('header').style.backgroundColor =
+            '#' + Math.floor(Math.random() * 16777215).toString(16);
     });
-  });
+
+    // Smooth scroll to section on nav link click
+    document.querySelectorAll('nav a').forEach((link) => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            document
+                .querySelector(this.getAttribute('href'))
+                .scrollIntoView({ behavior: 'smooth' });
+        });
+    });
 });
